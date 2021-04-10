f50e9bab586f8158724ccf3a04546a39712694e5
Thinassi <vthinassi@ciandt.com>
2021-04-10 18:38:03 -0300

    Completed module

--- logic/util.js
+++ logic/util.js
@@ -1,46 +1,46 @@
-// function Util() {
-//     // distributedPassengers = distributeAllSeatsToAllPassengers();
-    
-//     calculateTotalDistributedPassengers = (distributedPassengers) => {
-//         console.log(distributedPassengers);
-//         passengersSum = distributedPassengers.vipPassengersBusinessSeats + distributedPassengers.vipPassengersEconomySeats + distributedPassengers.regularPassengersBusinessSeats + distributedPassengers.regularPassengersEconomySeats
-//         return passengersSum;
-//     }
-
-//     calculateTotalNumberOfPassengers = (amountOfPassengers) => {
-//         const reducer = (accumulator, currentValue) => accumulator + currentValue;
-//         total = amountOfPassengers.reduce(reducer);
-//         console.log(total);
-//         return total
-//     }
-
-//     return { calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
-// }
-
-// module.exports = Util();
-
 function Util() {
-
-    function calculateTotalDistributedPassengers(distributedPassengers) {
-        let totalDistributedPassengers = 0;
-        let value;
-        for (value in distributedPassengers) {
-           totalDistributedPassengers += distributedPassengers[value];
-        }
+    // distributedPassengers = distributeAllSeatsToAllPassengers();
+    
+    calculateTotalDistributedPassengers = (distributedPassengers) => {
+        console.log(distributedPassengers);
+        totalDistributedPassengers = distributedPassengers.vipPassengersBusinessSeats + distributedPassengers.vipPassengersEconomySeats + distributedPassengers.regularPassengersBusinessSeats + distributedPassengers.regularPassengersEconomySeats
         return totalDistributedPassengers;
-     }
+    }
 
-     function calculateTotalNumberOfPassengers(passengersArray) {
-        let totalNumberOfPassengers = 0;
-        let passengers;
-        for (passengers of passengersArray) {
-           totalNumberOfPassengers += passengers;
-        }
-        return totalNumberOfPassengers;
-     }
+    calculateTotalNumberOfPassengers = (amountOfPassengers) => {
+        const reducer = (accumulator, currentValue) => accumulator + currentValue;
+        totalNumberOfPassengers = amountOfPassengers.reduce(reducer);
+        console.log(totalNumberOfPassengers);
+        return totalNumberOfPassengers
+    }
 
-     return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
-
+    return { calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
 }
 
-module.exports = Util(); 
+module.exports = Util();
+
+// function Util() {
+
+//     function calculateTotalDistributedPassengers(distributedPassengers) {
+//         let totalDistributedPassengers = 0;
+//         let value;
+//         for (value in distributedPassengers) {
+//            totalDistributedPassengers += distributedPassengers[value];
+//         }
+//         return totalDistributedPassengers;
+//      }
+
+//      function calculateTotalNumberOfPassengers(passengersArray) {
+//         let totalNumberOfPassengers = 0;
+//         let passengers;
+//         for (passengers of passengersArray) {
+//            totalNumberOfPassengers += passengers;
+//         }
+//         return totalNumberOfPassengers;
+//      }
+
+//      return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
+
+// }
+
+// module.exports = Util(); 
